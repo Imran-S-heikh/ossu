a6331f0b5ac813a1620cd21dbb7e32f179681985
Imran-S-heikh <imransheikhsadi@gmail.com>
2023-08-11 10:16:47 +0600

    HTCSD(string_image): Define String and Image in BSL

---
+++ how-to-code-simple-data/1A: begining-student-language/2.strings_and_images.rkt
@@ -0,0 +1,10 @@
+#lang racket
+; #reader
+; (lib "htdp-beginner-reader.ss" "lang")
+; ((modname strings_and_images) (read-case-sensitive #t) (teachpacks ()))
+(require 2htdp/image)
+
+(string-append "Ada" " " "Lovelace")
+
+
+(circle 10 "solid" "red")
